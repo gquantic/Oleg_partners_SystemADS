f14fbc8bf95278c76708b8edb219f6e027b6b10c
crazy-frontender <93494599+crazy-frontender@users.noreply.github.com>
2021-11-09 17:18:38 +0300

    Добавление формы вывода в dashboard

--- resources/views/main-pages/dashboard.blade.php
+++ resources/views/main-pages/dashboard.blade.php
@@ -82,30 +82,85 @@
                 </div>
             </div><!--end row-->
             <div class="row">
-                <div class="col-12 col-lg-12 col-xl-12 d-flex">
+                <div class="col-6 col-lg-6 col-xl-6 d-flex">
+                        <div class="card radius-10 w-100">
+                            <div class="card-header border-bottom bg-transparent">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <h6 class="mb-0">История входов</h6>
+                                    </div>
+                                    <div class="font-22 ms-auto"><i class="bx bx-dots-horizontal-rounded"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                @foreach($authorizationList as $authorize)
+                                    <li class="list-group-item bg-transparent">
+                                        <div class="d-flex align-items-center">
+                                            <div class="ms-0">
+                                                <h6 class="mb-0">{{ substr($authorize->device, 0, 36) }} <small class="ms-4">{{ str_replace('-', '.', $authorize->created_at); }}</small></h6>
+                                                <p class="mb-0 font-13 text-secondary">{{ $authorize->ip }}</p>
+                                            </div>
+                                            <hr>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                </div>
+                <div class="col-6 col-lg-6 col-xl-6 col-md-12 d-flex h-100">
                     <div class="card radius-10 w-100">
                         <div class="card-header border-bottom bg-transparent">
-                            <div class="d-flex align-items-center">
+                            <div class="d-flex align-items-center pt-2 pb-2">
                                 <div>
-                                    <h6 class="mb-0">История входов</h6>
+                                    <h6 class="mb-0">Заказать вывод средств</h6>
                                 </div>
-                                <div class="font-22 ms-auto"><i class="bx bx-dots-horizontal-rounded"></i>
+                                <div class="font-22 ms-auto">
                                 </div>
                             </div>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            @foreach($authorizationList as $authorize)
-                            <li class="list-group-item bg-transparent">
-                                <div class="d-flex align-items-center">
-                                    <div class="ms-0">
-                                        <h6 class="mb-0">{{ substr($authorize->device, 0, 36) }} <small class="ms-4">{{ str_replace('-', '.', $authorize->created_at); }}</small></h6>
-                                        <p class="mb-0 font-13 text-secondary">{{ $authorize->ip }}</p>
+                        <div class="card-body">
+                            <p>Доступная сумма к выводу составляет {{ Auth::user()->balance }}$</p>
+
+                            <form action="">
+                                @csrf
+
+                                <div class="form-group mb-3">
+                                    <label for="" class="mb-2">Сумма вывода</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="inputGroupPrepend">$</span>
+                                        </div>
+                                        <input type="text" name="amount" class="form-control">
                                     </div>
-                                    <hr>
                                 </div>
-                            </li>
-                            @endforeach
-                        </ul>
+
+                                <div class="form-group mb-3">
+                                    <label for="" class="mb-2">Вывести на</label>
+                                    <select name="" id="" class="form-control">
+                                        <option value="">Сбербанк</option>
+                                        <option value="">Qiwi</option>
+                                        <option value="">Webmoney</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="" class="mb-2">Реквизиты счета</label>
+                                    <input type="text" name="amount" class="form-control">
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label for="" class="mb-2">Пароль для подтверждения операции</label>
+                                    <input type="text" name="amount" class="form-control">
+                                </div>
+
+                                <p class="mb-2 mt-4">Нажав кнопку "Вывести" вы соглашаетесь с правилами и регламентом вывода средств.</p>
+
+                                <button class="btn btn-primary">
+                                    Вывести
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
