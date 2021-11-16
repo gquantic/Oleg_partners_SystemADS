b1235e75e67f22d45e6cdf2ecffa5ff3a01d9f99
crazy-frontender <93494599+crazy-frontender@users.noreply.github.com>
2021-11-16 21:33:32 +0300

    Update settings.blade.php

--- resources/views/main-pages/settings.blade.php
+++ resources/views/main-pages/settings.blade.php
@@ -68,7 +68,7 @@
                                         <img src="https://e7.pngegg.com/pngimages/165/652/png-clipart-businessperson-computer-icons-avatar-avatar-heroes-public-relations.png" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
                                         <div class="mt-3">
                                             <h4>{{ Auth::user()->name }}</h4>
-                                            <h6 class="text-secondary mb-1 ">Admin</h6>
+                                            <!--h6 class="text-secondary mb-1 ">Admin</h6-->
                                             {{--                                            <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>--}}
                                             <button class="btn btn-primary">Изменить аватар</button>
                                             {{--                                            <button class="btn btn-outline-primary">Message</button>--}}
@@ -80,10 +80,10 @@
                                             <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail me-2 icon-inline text-primary"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Email</h6>
                                             <span class="text-secondary">{{ Auth::user()->email }}</span>
                                         </li>
-                                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                        <!--li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                             <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook me-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
                                             <span class="text-secondary">codervent</span>
-                                        </li>
+                                        </li-->
                                     </ul>
                                 </div>
                             </div>
@@ -169,7 +169,7 @@
                                             </form>
                                         </div>
                                     </div>
-                                    <div class="col-lg-12">
+                                    <!--div class="col-lg-12">
                                         <div class="card">
                                             <div class="card-header text-center">
                                                 <strong>Смена Email</strong>
@@ -205,7 +205,7 @@
                                                     </button>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div-->
                                     </div>
                                 </div>
                             </div>
